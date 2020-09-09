cd10cbb5031f7c17849bcf1147e4bd9b887ce411
Emanuel Toledo <ematolr@gmail.com>
2020-09-09 15:12:35 -0500

    practicing state

--- react-complete-guide-example/src/App.js
+++ react-complete-guide-example/src/App.js
@@ -3,12 +3,27 @@ import './App.css';
 import Person from './Person/Person.js'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 23 },
+      { name: 'Steph', age: 38 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked.');
+  }
+
   render() {
     return (
       <div className="App">
        <h1>Hi, I'm a react app.</h1>
        <p>IT is working!</p>
-       <Person />
+       <button onClick={this.switchNameHandler}> Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My hobbies: Racing </Person>
+       <Person name= {this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does it work?'));
@@ -16,3 +31,4 @@ class App extends Component {
 }
 
 export default App;
+ 
